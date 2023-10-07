468d4ade9575062ed15ecf6a84cd7674c0aff796
Xuty <xutyxd@gmail.com>
2023-10-07 17:07:48 +0200

    fix(exports): Fixed bug with importations, now support node & web with es6 and commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 
-import { IReader, IWriter } from "./interfaces"
+import { IReader, IWriter } from './interfaces'
 
-export { IReader, IWriter }
+import { WebReader } from './classes/readers/web.reader.class';
+import { WebWriter } from './classes/writers/web.writer.class';
+
+const isNode = typeof process !== "undefined" && process.versions != null && process.versions.node != null;
+const isMJS = typeof require === 'undefined';
+// @ts-ignore
+const { NodeReader } = isNode && (isMJS ? await import('./classes/readers/node.reader.class.js') : eval(`require('./classes/readers/node.reader.class.js')`));
+// @ts-ignore
+const { NodeWriter } = isNode && (isMJS ? await import('./classes/writers/node.writer.class.js') : eval(`require('./classes/writers/node.writer.class.js')`));
+
+export { IReader, IWriter, WebReader, WebWriter, NodeWriter, NodeReader }
+
