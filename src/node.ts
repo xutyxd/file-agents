9d790c3d19bf9d7484068255f0e02e3763b8d68d
Xuty <xutyxd@gmail.com>
2023-10-07 17:36:32 +0200

    fix(cleanup): Removed unused files

--- src/node.ts
+++
@@ -1,5 +0,0 @@
-
-import { NodeReader } from "./classes/readers/node.reader.class";
-import { NodeWriter } from "./classes/writers/node.writer.class";
-
-export { NodeReader, NodeWriter };
--- src/web.ts
+++
@@ -1,5 +0,0 @@
-
-import { WebReader } from "./classes/readers/web.reader.class";
-import { WebWriter } from "./classes/writers/web.writer.class";
-
-export { WebReader, WebWriter };
