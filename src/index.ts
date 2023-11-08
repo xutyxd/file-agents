5558051dec41345ad61f6a85cf2dcf2809614cdd
Xuty <xutyxd@gmail.com>
2023-11-08 23:26:17 +0100

    feat(require): Try new way of avoid tla

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 
+import { createRequire } from "module";
+
 import { IReader, IWriter } from './interfaces'
 
 import { WebReader } from './classes/readers/web.reader.class';
@@ -6,13 +8,17 @@ import { WebWriter } from './classes/writers/web.writer.class';
 import type { NodeReader as INodeReader } from './classes/readers/node.reader.class';
 import type { NodeWriter as INodeWriter } from './classes/writers/node.writer.class';
 const isNode = typeof process !== "undefined" && process.versions != null && process.versions.node != null;
-const isMJS = typeof require === 'undefined';
-// @ts-ignore
-const { NodeReader: Reader } = (isNode && (isMJS ? await import('./classes/readers/node.reader.class.js') : eval(`require('./classes/readers/node.reader.class.js')`))) as INodeReader;
-// @ts-ignore
-const { NodeWriter: Writer } = (isNode && (isMJS ? await import('./classes/writers/node.writer.class.js') : eval(`require('./classes/writers/node.writer.class.js')`))) as INodeWriter;
-const NodeReader: typeof INodeReader = Reader;
-const NodeWriter: typeof INodeWriter = Writer;
+
+let NodeReader: typeof INodeReader;
+let NodeWriter: typeof INodeWriter;
+
+if (isNode) {
+    const get = require || createRequire && createRequire(eval(`import.meta.url`));
+    const { NodeReader: Reader } = (isNode && get('./classes/readers/node.reader.class.js'));
+    const { NodeWriter: Writer } = (isNode && get('./classes/readers/node.writer.class.js'));
+    NodeReader = Reader;
+    NodeWriter = Writer;
+}
 
 export { IReader, IWriter, WebReader, WebWriter, NodeWriter, NodeReader }
 
